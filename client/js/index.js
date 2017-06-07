3c8a4a5350462df36208d8e0f1dd1a8e1d4883fa
Jose Sales <salesbass@gmail.com>
2017-06-07 16:13:59 -0300

    initial set up

---
+++ client/js/index.js
@@ -0,0 +1,7 @@
+var fields = [
+              document.querySelector("#date"),
+              document.querySelector("#amount"),
+              document.querySelector("#value"),
+              ];
+
+console.log(fields);

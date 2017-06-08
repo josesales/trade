25c4aa33ff93511d84b3033ca4b7b7583058e812
U-THE_RAIN\pc <salesbass@gmail.com>
2017-06-07 17:29:07 -0700

    Put function

--- client/js/index.js
+++ client/js/index.js
@@ -1,7 +1,36 @@
 var fields = [
-              document.querySelector("#date"),
-              document.querySelector("#amount"),
-              document.querySelector("#value"),
-              ];
+    document.querySelector("#date"),
+    document.querySelector("#amount"),
+    document.querySelector("#value"),
+];
 
 console.log(fields);
+var tBody = document.querySelector("table tbody");
+
+var cleanFields = function (fields) {
+    fields[0].value = '';
+    fields[1].value = 1;
+    fields[2].value = 0;
+
+    fields[0].focus();
+};
+
+//Insert the datas in the grid
+document.querySelector('.form').addEventListener('submit', function (event) {
+
+    event.preventDefault();
+    var tr = document.createElement('tr');
+
+    fields.forEach(function (field) {
+        var td = document.createElement('td');
+        td.textContent = field.value;
+        tr.appendChild(td);
+    });
+
+    var tdTotalAmount = document.createElement('td');
+    tdTotalAmount.textContent = fields[1].value * fields[2].value;
+    tr.appendChild(tdTotalAmount);
+
+    tBody.appendChild(tr);
+    cleanFields(fields);
+});

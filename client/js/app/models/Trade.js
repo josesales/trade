841b401e7d00f21cc67733dbd7b9f5cd319fb658
U-THE_RAIN\pc <salesbass@gmail.com>
2017-06-07 21:19:11 -0700

    Creating of the Trade model using EC6

---
+++ client/js/app/models/Trade.js
@@ -0,0 +1,22 @@
+class Trade {
+    constructor(date, amount, value) {
+        this._date = new Date(this.date.getTime());
+        this._amount = amount;
+        this._value = value;
+        Object.freeze(this);
+    }
+
+    getTotalValue() {
+        return this.amount * this.value;
+    }
+
+    get date() {
+        return new Date(this._date.getTime());
+    }
+    get amount() {
+        return this._amount;
+    }
+    get value() {
+        return this._value;
+    }
+}
